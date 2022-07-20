273ab96be6340b4e4b6212459059d67cb7a62008
VladimirForsend <75955208+vtorresmz@users.noreply.github.com>
2022-07-19 20:25:45 -0400

    modificación del header

--- header.php
+++ header.php
@@ -1,35 +1,41 @@
 <?php
+
 /**
- * The header for our theme
+ * The header for our hola marcos 
  *
  * This is the template that displays all of the <head> section and everything up until <div id="content">
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package museo-ferroviario
+ * @package ecommerce
  */
 
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+
+    <?php wp_head(); ?>
+
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'museo-ferroviario' ); ?></a>
 
-	<header id="masthead" class="site-header" d-none d-md-block>
-	<?php 
-include get_template_directory() . '/assets/templates/navs/nav-desk.php';?>
-	</header> <!---#masthead -->
-	<header id="masthead" class="site-header" d-block d-md-none >
-	<?php 
-include get_template_directory() . '/assets/templates/navs/nav-mobile.php';?>
-	</header> <!---#masthead -->
+    <?php wp_body_open(); ?>
+    <div id="page" class="site sticky-top nav-shadow">
+        <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'ecommerce-para-chile'); ?></a>
+
+        <header id="masthead" class="site-header comercio-header">
+        <div class="d-none d-md-block d-lg-block d-xl-block">
+        <?php include get_template_directory() . '/assets/templates/navs/nav-desk.php'; ?>    
+        </div>
+        <div class="d-block d-md-none d-lg-none d-xl-none">
+        <?php include get_template_directory() . '/assets/templates/navs/nav-mobile.php'; ?>
+        </div>    
+    </header><!-- #masthead -->
+        <?php /* include get_template_directory() . '/preloader.php'; */ ?>
+    </div>
