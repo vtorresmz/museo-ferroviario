e5204bf34753c0cfaaa860354a2fc7a71c53c7f3
carladdw <74931569+carladdw@users.noreply.github.com>
2022-07-20 19:09:09 -0400

    cambio de asset

--- assets/assets.php
+++ assets/assets.php
@@ -12,13 +12,13 @@ add_filter( 'use_widgets_block_editor', '__return_false' );
 function titan_framework()
 {
     wp_register_style('fuentes', 'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap', 'all');
-    wp_enqueue_style('iconos', get_template_directory_uri() . '/assets/librerias/fafa/css/all.css', 'all');
+    wp_register_style('iconos', get_template_directory_uri() . '/assets/librerias/fafa/css/all.css', 'all');
     wp_register_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css', 'all');
-    wp_enqueue_style('root', get_template_directory_uri() . '/assets/librerias/css/root.css', 'all');
-    wp_enqueue_style('base', get_template_directory_uri() . '/assets/librerias/css/base.css', 'all');
-    wp_enqueue_style('museo', get_template_directory_uri() . '/assets/librerias/css/museo.css', 'all');
-    wp_enqueue_style('slider-fluid', get_template_directory_uri() . '/assets/librerias/css/slider-fluid.css', 'all');
-    wp_enqueue_style('mobile', get_template_directory_uri() . '/assets/librerias/css/mobile.css', 'all');
+    wp_register_style('root', get_template_directory_uri() . '/assets/librerias/css/root.css', 'all');
+    wp_register_style('base', get_template_directory_uri() . '/assets/librerias/css/base.css', 'all');
+    wp_register_style('museo', get_template_directory_uri() . '/assets/librerias/css/museo.css', 'all');
+    wp_register_style('slider-fluid', get_template_directory_uri() . '/assets/librerias/css/slider-fluid.css', 'all');
+    wp_register_style('mobile', get_template_directory_uri() . '/assets/librerias/css/mobile.css', 'all');
 
    
 
